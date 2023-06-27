4bb5a6fac667c923cc48b87e823c42154c6dc40b
Renato Almeida <renato.almeida@ydata.ai>
2023-06-27 14:52:12 +0100

    Changes according to review

--- Sources/ElasticSwift/Requests/PointInTimeRequest.swift
+++ Sources/ElasticSwift/Requests/PointInTimeRequest.swift
@@ -1,44 +1,41 @@
 import ElasticSwiftCore
-import NIOHTTP1
 import Foundation
+import NIOHTTP1
 
 // MARK: - Point In Time Request
 
-public struct PointInTimeRequest: Request {
-  public var headers = HTTPHeaders()
+public struct PointInTimeRequest: Request, Equatable {
+  public let indices: [String]
+  public let keepAlive: String
 
-    public let indices: [String]
-    public let keepAlive: String 
+  public init(indices: [String], keepAlive: String) {
+    self.indices = indices
+    self.keepAlive = keepAlive
+  }
 
-    public init(indices: [String], keepAlive: String){
-       self.indices = indices
-       self.keepAlive = keepAlive 
+  public init(indices: String..., keepAlive: String) {
+    self.init(indices: indices, keepAlive: keepAlive)
+  }
+
+  public var method: HTTPMethod {
+    return .POST
+  }
+
+  public var endPoint: String {
+    let pitEndPoint = "_pit"
+
+    guard !indices.isEmpty else {
+        return pitEndPoint
     }
 
-    public init(indices: String..., keepAlive: String){
-        self.init(indices: indices, keepAlive: keepAlive)
-    }
+    return indices.joined(separator: ",") + "/" + pitEndPoint
+  }
 
-    public var method: HTTPMethod {
-        return .POST
-    }
+  public var queryParams: [URLQueryItem] {
+    return [URLQueryItem(name: QueryParams.keepAlive, value: keepAlive)]
+  }
 
-    public var endPoint: String {
-        var _endPoint = "_pit"
-        if !indices.isEmpty {
-            _endPoint = indices.joined(separator: ",") + "/" + _endPoint
-        }
-        return _endPoint
-    }
-
-    public var queryParams: [URLQueryItem] {
-        return [URLQueryItem(name: QueryParams.keepAlive, value: keepAlive)]
-    }
-
-    public func makeBody(_: Serializer) -> Result<Data, MakeBodyError> {
-        return .failure(MakeBodyError.noBodyForRequest)
-    }
+  public func makeBody(_: Serializer) -> Result<Data, MakeBodyError> {
+    return .failure(MakeBodyError.noBodyForRequest)
+  }
 }
-
-extension PointInTimeRequest: Equatable {}
-
