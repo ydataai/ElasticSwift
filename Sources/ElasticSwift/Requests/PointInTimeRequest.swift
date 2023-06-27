47b9336e84e9879dc12cc756e96bf90706c0aa4a
Renato Almeida <renato.almeida@ydata.ai>
2023-06-27 15:23:16 +0100

    fix indentation to match project

--- Sources/ElasticSwift/Requests/PointInTimeRequest.swift
+++ Sources/ElasticSwift/Requests/PointInTimeRequest.swift
@@ -5,39 +5,39 @@ import NIOHTTP1
 // MARK: - Point In Time Request
 
 public struct PointInTimeRequest: Request, Equatable {
-  public var headers = HTTPHeaders()
+    public var headers = HTTPHeaders()
 
-  public let indices: [String]
-  public let keepAlive: String
+    public let indices: [String]
+    public let keepAlive: String
 
-  public init(indices: [String], keepAlive: String) {
-    self.indices = indices
-    self.keepAlive = keepAlive
-  }
-
-  public init(indices: String..., keepAlive: String) {
-    self.init(indices: indices, keepAlive: keepAlive)
-  }
-
-  public var method: HTTPMethod {
-    return .POST
-  }
-
-  public var endPoint: String {
-    let pitEndPoint = "_pit"
-
-    guard !indices.isEmpty else {
-        return pitEndPoint
+    public init(indices: [String], keepAlive: String) {
+        self.indices = indices
+        self.keepAlive = keepAlive
     }
 
-    return indices.joined(separator: ",") + "/" + pitEndPoint
-  }
+    public init(indices: String..., keepAlive: String) {
+        self.init(indices: indices, keepAlive: keepAlive)
+    }
 
-  public var queryParams: [URLQueryItem] {
-    return [URLQueryItem(name: QueryParams.keepAlive, value: keepAlive)]
-  }
+    public var method: HTTPMethod {
+        return .POST
+    }
 
-  public func makeBody(_: Serializer) -> Result<Data, MakeBodyError> {
-    return .failure(MakeBodyError.noBodyForRequest)
-  }
+    public var endPoint: String {
+        let pitEndPoint = "_pit"
+
+        guard !indices.isEmpty else {
+            return pitEndPoint
+        }
+
+        return indices.joined(separator: ",") + "/" + pitEndPoint
+    }
+
+    public var queryParams: [URLQueryItem] {
+        return [URLQueryItem(name: QueryParams.keepAlive, value: keepAlive)]
+    }
+
+    public func makeBody(_: Serializer) -> Result<Data, MakeBodyError> {
+        return .failure(MakeBodyError.noBodyForRequest)
+    }
 }
